e7bf65e74ea423d3933bdfcb1a79445c44f46dcc
Miha Fuderer <m.fuderer@umcutrecht.nl>
2025-04-14 10:44:09 +0200

    switch off intermediate plots

--- src/MRSTAT.jl
+++ src/MRSTAT.jl
@@ -32,6 +32,7 @@ function mrstat_recon(
     LB=T₁T₂ρˣρʸ(log(0.1), log(0.001), -Inf, -Inf),
     UB=T₁T₂ρˣρʸ(log(7.0), log(3.000), Inf, Inf),
     trf_options=TrustRegionReflective.SolverOptions(),
+    intermediate_plots =true
 ) where {T<:Complex}
 
     # Repeat the initial guess and bounds for each voxel
@@ -47,9 +48,8 @@ function mrstat_recon(
     resource = CUDALibs()
     objfun = (x, mode) -> objective(x, resource, mode, raw_data, sequence, coordinates, coil_sensitivities, trajectory)
 
-    plotfun(x, figtitle) = plot_T₁T₂ρ(optim_to_physical_pars(x), isqrt(num_voxels), isqrt(num_voxels), figtitle)
+    plotfun(x, figtitle) = intermediate_plots ? plot_T₁T₂ρ(optim_to_physical_pars(x), isqrt(num_voxels), isqrt(num_voxels), figtitle) : nothing
     plotfun(x0, "Initial Guess")
-    # plotfun(x, figtitle) = println("Not plotting anything")
 
     output = TrustRegionReflective.solver(objfun, vec(x0), vec(LB), vec(UB), trf_options, plotfun)
 
